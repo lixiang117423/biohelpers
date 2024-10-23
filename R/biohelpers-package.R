11cd8708f5860f4f382b059bbf74cc329c87a22b
lixiang <lixiang117423@gmail.com>
2024-10-23 21:09:53 +0800

    modify PCAinALL function

---
+++ R/biohelpers-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom base factor
+#' @importFrom base paste0
+#' @importFrom base round
+#' @importFrom base unique
+#' @importFrom dplyr arrange
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom factoextra get_eigenvalue
+#' @importFrom FactoMineR PCA
+## usethis namespace: end
+NULL
--- deve.log.R
+++ deve.log.R
@@ -1,5 +1,3 @@
-# 
-
 # add MIT license
 usethis::use_mit_license()
 
@@ -7,5 +5,19 @@ usethis::use_mit_license()
 devtools::check()
 
 # add function PCAinALL
-usethis::use_r("PCAinALL")
+# usethis::use_r("PCAinALL")
 styler::style_file("R/PCAinALL.R")
+usethis::use_package("FactoMineR")
+devtools::document()
+
+# 添加用到的R包和其中的部分功能
+# 会自动添加到R/biohelpers-package.R这个文件中
+usethis::use_import_from("FactoMineR", "PCA")
+usethis::use_import_from("factoextra", "get_eigenvalue")
+usethis::use_import_from("dplyr", "mutate")
+usethis::use_import_from("dplyr", "arrange")
+usethis::use_import_from("base", "paste0")
+usethis::use_import_from("base", "factor")
+usethis::use_import_from("base", "unique")
+usethis::use_import_from("base", "round")
+usethis::use_import_from("dplyr", "select")
